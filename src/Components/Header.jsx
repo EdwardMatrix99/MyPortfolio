b7d46761afc3e6979bf8878d9004e53bda48c930
Edward <141988488+EdwardMatrix99@users.noreply.github.com>
2025-03-25 22:01:01 -0500

    nuevos cambios en el header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -47,7 +47,7 @@ const Header = () => {
         <ul className="lista">
 
           <li className="nav-item">
-            <a className="enlace" id='casa' href="#about">Home</a>
+            <a className="enlace" id='casa' href="#about">  Casa </a>
           </li>
           <li className="nav-item">
             <a className="enlace" href="#projects">Projects</a>
